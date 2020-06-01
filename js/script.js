3839b0ad728af48ff9a95d768f2590f1dc76b2a7
Udjin Stolboviy <UdjinStolbovy@gmail.com>
2020-06-01 22:27:20 +0300

    Lesson 2.10 home work

--- js/script.js
+++ js/script.js
@@ -1,131 +1,242 @@
 'use strict'
 
 
-let many = prompt("Ваш бюджет на месяц ?"),
-    time = prompt("Введите дату в формате YYYY - MM - DD ");
+// let many = prompt("Ваш бюджет на месяц ?", ''),
+//     time = prompt("Введите дату в формате YYYY - MM - DD ", '');
 
+
+// let appData = {
+//     manyAll: many,
+//     dataTime: time,
+//     expenses: { 
+//         anw1: 'ответ на первый вопрос',
+//         anw2: 'ответ на второй вопрос'
+//     },
+//     optionalExpenses: {},
+//     income: [],
+//     savings: false
+// };
+
+// let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+//     a2 = prompt("Во сколько обойдется?", ''),
+//     a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+//     a4 = prompt("Во сколько обойдется?", '');
+
+// appData.expenses.a1 = a2;
+// appData.expenses.a3 = a4;
+
+// alert(appData.budget / 30);
+
+// let num = 50;
+
+// if (num < 49) {
+//     console.log('Неверно!')
+// } else if (num > 100) {
+//     console.log('Много!')
+// } else {
+//     console.log ('Верно!')
+// };
+
+// (num == 50) ? console.log('Верно!') : console.log('НЕверно!');
+
+// switcw (num) {
+//     case num < 49:
+//         console.log('Неверно!');
+//         break;
+//     case num > 100:
+//     console.log('Много!');
+//     break;
+//     case num > 80:
+//     console.log('Все ещо много!');
+//     break;
+//     case 50;
+//     console.log('Верно!');
+//     break;
+//     default:
+//     console.log('Что то пошло не  так!');
+    
+
+// }
+
+
+'use strict';
+
+
+let money = +prompt("Ваш бюджет на месяц?", ""),
+    time = prompt("Введите дату в формате YYYY-MM-DD", "");
 
 let appData = {
-    manyAll: many,
-    dataTime: time,
+    budget: money,
+    timeData: time,
     expenses: {},
     optionalExpenses: {},
     income: [],
     savings: false
 };
-// let expenses: {
-//     "ответ на первый вопрос": "ответ на второй вопрос"
+
+
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+        b = prompt("Во сколько обойдется?", "");
+
+    if (typeof (a) === 'string' && typeof (a) != null && typeof (b) != null && a != "" && b != "" && a.length < 50) {
+
+        console.log("done");
+
+        appData.expenses[a] = b;
+    } else {
+        console.log("bad result");
+        i--;
+    }
+
+};
+
+
+
+// Используем цикл WHILE
+
+// let i = 0;
+// while (i < 2) {
+//     let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
+//         b = prompt ("Во сколько обойдется?", "");
+
+//     if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
+
+//         console.log ("done");
+
+//         appData.expenses[a] = b;
+//     } else {
+//          console.log ("bad result");
+//          i--;
+//     }
+
+//     i++;
 // }
 
 
 
+// Используем цикл DO...WHILE
 
+// let i = 0;
+// do {
+//     let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
+//         b = prompt ("Во сколько обойдется?", "");
 
+//     if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
 
+//         console.log ("done");
 
+//         appData.expenses[a] = b;
+//     } else {
+//          console.log ("bad result");
+//          i--;
+//     }
 
+//     i++;
+// }
+// while(i < 2);
 
 
+appData.moneyPerDay = appData.budget / 30;
 
 
+alert("Бюджет на 1 день составляет " + appData.moneyPerDay + "руб.");
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-    
-    
-
-function testWebP(callback) {
-
-    let webP = new Image();
-    webP.onload = webP.onerror = function () {
-        callback(webP.height == 2);
-    };
-    webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+if (appData.moneyPerDay < 100) {
+    console.log("Это минимальный уровень достатка!");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("Это средний уровень достатка!");
+} else if (appData.moneyPerDay > 2000) {
+    console.log("Это высокий уровень достатка!");
+} else {
+    console.log("Произошла ошибка");
 }
 
-testWebP(function (support) {
 
-    if (support == true) {
-        document.querySelector('body').classList.add('webp');
-    } else {
-        document.querySelector('body').classList.add('no-webp');
-    }
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
